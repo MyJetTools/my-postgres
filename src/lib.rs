c163bb7d34fa96cfbbb25a30fc58c32c651f84f3
amigin <amigin@gmail.com>
2022-07-05 17:24:21 +0300

    Added ObjectPool

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 pub mod code_gens;
 mod my_postgres;
+mod objects_pool;
 mod traits;
 pub use my_postgres::MyPostgres;
+pub use objects_pool::*;
 pub use traits::*;
---
+++ src/objects_pool/mod.rs
@@ -0,0 +1,6 @@
+mod object_pool;
+mod object_pool_inner;
+mod rented_object;
+pub use object_pool::ObjectsPool;
+pub use object_pool_inner::ObjectPoolInner;
+pub use rented_object::RentedObject;
