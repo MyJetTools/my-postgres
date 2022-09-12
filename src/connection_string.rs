749961944d958f768f2c2a1fd21d963c9eb4c8c0
amigin <amigin@gmail.com>
2022-09-12 11:36:11 +0300

    Added SSL Mode removed

--- src/connection_string.rs
+++ src/connection_string.rs
@@ -10,13 +10,109 @@ pub struct ConnectionString<'s> {
     user_name: Position,
     password: Position,
     host: Position,
-    port: Position,
+    port: u32,
     db_name: Position,
     ssl_require: bool,
 }
 
 impl<'s> ConnectionString<'s> {
-    pub fn parse(conn_string: &'s [u8]) -> Self {
+    pub fn parse(conn_string: ConnectionStringFormat<'s>) -> Self {
+        match conn_string {
+            ConnectionStringFormat::ReadyToUse(_) => {
+                panic!("We should not go here");
+            }
+            ConnectionStringFormat::AsUrl(conn_string) => {
+                Self::parse_url_connstring(conn_string.as_bytes())
+            }
+            ConnectionStringFormat::SemiColumnSeparated(conn_string) => {
+                Self::parse_column_separated(conn_string.as_bytes())
+            }
+        }
+    }
+
+    fn parse_column_separated(conn_string: &'s [u8]) -> Self {
+        let mut user_name = None;
+        let mut password = None;
+        let mut db_name = None;
+        let mut host = None;
+        let port: u32 = 5432;
+        let mut ssl_require = false;
+
+        let mut pos = 0;
+        while pos < conn_string.len() {
+            let eq_pos = find_pos(conn_string, pos + 1, b'=');
+
+            if eq_pos == None {
+                break;
+            }
+
+            let eq_pos = eq_pos.unwrap();
+
+            let end_of_value = find_pos(conn_string, eq_pos + 1, b';');
+
+            let end_of_value = match end_of_value {
+                Some(end_of_value) => end_of_value,
+                None => conn_string.len(),
+            };
+
+            let key = std::str::from_utf8(&conn_string[pos..eq_pos])
+                .unwrap()
+                .trim()
+                .to_lowercase();
+
+            match key.as_str() {
+                "server" => {
+                    host = Some(Position {
+                        from: eq_pos + 1,
+                        to: end_of_value,
+                    });
+                }
+                "userid" => {
+                    user_name = Some(Position {
+                        from: eq_pos + 1,
+                        to: end_of_value,
+                    });
+                }
+                "password" => {
+                    password = Some(Position {
+                        from: eq_pos + 1,
+                        to: end_of_value,
+                    });
+                }
+
+                "database" => {
+                    db_name = Some(Position {
+                        from: eq_pos + 1,
+                        to: end_of_value,
+                    });
+                }
+
+                "sslmode" => {
+                    let value = std::str::from_utf8(&conn_string[eq_pos + 1..end_of_value])
+                        .unwrap()
+                        .trim()
+                        .to_lowercase();
+                    ssl_require = value == "require";
+                }
+
+                _ => {}
+            }
+
+            pos = end_of_value + 1;
+        }
+
+        Self {
+            conn_string: conn_string,
+            user_name: user_name.unwrap(),
+            password: password.unwrap(),
+            host: host.unwrap(),
+            port,
+            db_name: db_name.unwrap(),
+            ssl_require,
+        }
+    }
+
+    fn parse_url_connstring(conn_string: &'s [u8]) -> Self {
         let user_name_start = PREFIX.len();
 
         let pos = find_pos(conn_string, user_name_start + 1, b':');
@@ -55,6 +151,11 @@ impl<'s> ConnectionString<'s> {
 
         let port_end = pos.unwrap();
 
+        let port = std::str::from_utf8(&conn_string[port_start..port_end])
+            .unwrap()
+            .parse::<u32>()
+            .unwrap();
+
         let db_name_start = port_end + 1;
 
         let mut ssl_require = false;
@@ -90,10 +191,7 @@ impl<'s> ConnectionString<'s> {
                 from: host_start,
                 to: host_end,
             },
-            port: Position {
-                from: port_start,
-                to: port_end,
-            },
+            port,
             db_name: Position {
                 from: db_name_start,
                 to: db_name_end,
@@ -111,7 +209,7 @@ impl<'s> ConnectionString<'s> {
             format!(
                 "host={} port={} dbname={} user={} password={} application_name={} sslmode=require",
                 self.get_field_value(&self.host),
-                self.get_field_value(&self.port),
+                &self.port,
                 self.get_field_value(&self.db_name),
                 self.get_field_value(&self.user_name),
                 self.get_field_value(&self.password),
@@ -121,7 +219,7 @@ impl<'s> ConnectionString<'s> {
             format!(
                 "host={} port={} dbname={} user={} password={} application_name={}",
                 self.get_field_value(&self.host),
-                self.get_field_value(&self.port),
+                &self.port,
                 self.get_field_value(&self.db_name),
                 self.get_field_value(&self.user_name),
                 self.get_field_value(&self.password),
@@ -144,24 +242,70 @@ pub fn find_pos(src: &[u8], start_pos: usize, search_byte: u8) -> Option<usize> 
 pub fn format(conn_string: &str, app_name: &str) -> String {
     let conn_string = conn_string.trim();
 
-    if conn_string.starts_with(PREFIX) {
-        let conn_string = ConnectionString::parse(conn_string.as_bytes());
-        return conn_string.to_string(app_name);
+    let conn_string_format = ConnectionStringFormat::parse_and_detect(conn_string);
+
+    if conn_string_format.is_ready_to_use() {
+        return format!("{} application_name={}", conn_string, app_name);
     }
 
-    format!("{} application_name={}", conn_string, app_name)
+    let conn_string = ConnectionString::parse(conn_string_format);
+
+    conn_string.to_string(app_name)
+}
+
+pub enum ConnectionStringFormat<'s> {
+    ReadyToUse(&'s str),
+    AsUrl(&'s str),
+    SemiColumnSeparated(&'s str),
+}
+
+impl<'s> ConnectionStringFormat<'s> {
+    pub fn parse_and_detect(conn_string: &'s str) -> ConnectionStringFormat {
+        if conn_string.trim().starts_with(PREFIX) {
+            return ConnectionStringFormat::AsUrl(conn_string);
+        }
+
+        let mut spaces = 0;
+
+        let mut semicolumns = 0;
+
+        let as_bytes = conn_string.as_bytes();
+
+        for i in 0..as_bytes.len() {
+            if as_bytes[i] == b' ' {
+                spaces += 1;
+            } else if as_bytes[i] == b';' {
+                semicolumns += 1;
+            }
+        }
+
+        if spaces > semicolumns {
+            return ConnectionStringFormat::ReadyToUse(conn_string);
+        }
+
+        return ConnectionStringFormat::SemiColumnSeparated(conn_string);
+    }
+
+    pub fn is_ready_to_use(&'s self) -> bool {
+        match self {
+            ConnectionStringFormat::ReadyToUse(_) => true,
+            _ => false,
+        }
+    }
 }
 
 #[cfg(test)]
 mod test {
 
-    use super::ConnectionString;
+    use super::*;
 
     #[test]
     fn test_connstring_with_no_additional_params() {
-        let connection_string = ConnectionString::parse(
-            "postgresql://username@username:password@localhost:5432/dbname".as_bytes(),
+        let conn_string_format = ConnectionStringFormat::parse_and_detect(
+            "postgresql://username@username:password@localhost:5432/dbname",
         );
+
+        let connection_string = ConnectionString::parse(conn_string_format);
 
         assert_eq!(
             "username@username",
@@ -178,10 +322,7 @@ mod test {
             connection_string.get_field_value(&connection_string.host)
         );
 
-        assert_eq!(
-            "5432",
-            connection_string.get_field_value(&connection_string.port)
-        );
+        assert_eq!(5432, connection_string.port);
 
         assert_eq!(
             "dbname",
@@ -193,10 +334,10 @@ mod test {
 
     #[test]
     fn test_parsing_csharp_like_connection_string() {
-        let connection_string = ConnectionString::parse(
-            "postgresql://username@username:password@localhost:5432/dbname?sslmode=require"
-                .as_bytes(),
+        let conn_string_format = ConnectionStringFormat::parse_and_detect(
+            "postgresql://username@username:password@localhost:5432/dbname?sslmode=require",
         );
+        let connection_string = ConnectionString::parse(conn_string_format);
 
         assert_eq!(
             "username@username",
@@ -213,10 +354,7 @@ mod test {
             connection_string.get_field_value(&connection_string.host)
         );
 
-        assert_eq!(
-            "5432",
-            connection_string.get_field_value(&connection_string.port)
-        );
+        assert_eq!(5432, connection_string.port);
 
         assert_eq!(
             "dbname",
@@ -227,10 +365,11 @@ mod test {
     }
 
     #[test]
-    fn test_parsing_csharp_like_connection_string_with_timeout() {
-        let connection_string = ConnectionString::parse(
-            "postgresql://admin:example@10.0.0.3:5432/mydbname?connect_timeout=10".as_bytes(),
+    fn test_parsing_url_like_connection_string_with_timeout() {
+        let conn_string_format = ConnectionStringFormat::parse_and_detect(
+            "postgresql://admin:example@10.0.0.3:5432/mydbname?connect_timeout=10",
         );
+        let connection_string = ConnectionString::parse(conn_string_format);
 
         assert_eq!(
             "admin",
@@ -247,10 +386,7 @@ mod test {
             connection_string.get_field_value(&connection_string.host)
         );
 
-        assert_eq!(
-            "5432",
-            connection_string.get_field_value(&connection_string.port)
-        );
+        assert_eq!(5432, connection_string.port);
 
         assert_eq!(
             "mydbname",
@@ -258,5 +394,37 @@ mod test {
         );
 
         assert_eq!(false, connection_string.ssl_require,);
+    }
+
+    #[test]
+    fn test_parsing_semicolon_separated_connection_string() {
+        let conn_string_format = ConnectionStringFormat::parse_and_detect(
+            "Server=localhost;UserId=usr;Password=pswd;Database=payments;sslmode=require",
+        );
+        let connection_string = ConnectionString::parse(conn_string_format);
+
+        assert_eq!(
+            "usr",
+            connection_string.get_field_value(&connection_string.user_name)
+        );
+
+        assert_eq!(
+            "pswd",
+            connection_string.get_field_value(&connection_string.password)
+        );
+
+        assert_eq!(
+            "localhost",
+            connection_string.get_field_value(&connection_string.host)
+        );
+
+        assert_eq!(5432, connection_string.port);
+
+        assert_eq!(
+            "payments",
+            connection_string.get_field_value(&connection_string.db_name)
+        );
+
+        assert_eq!(true, connection_string.ssl_require,);
     }
 }
