2b74fe5f47625f6a261b737bdd9284f3cd8b8ee8
amigin <amigin@gmail.com>
2022-11-23 14:06:15 +0200

    Added In Support

--- src/input_data_value.rs
+++ src/input_data_value.rs
@@ -14,4 +14,17 @@ pub enum InputDataValue<'s> {
         op: &'static str,
         value: &'s (dyn tokio_postgres::types::ToSql + Sync),
     },
+    AsInOfString {
+        name: &'static str,
+        values: Vec<String>,
+    },
+    AsInOfNonString {
+        name: &'static str,
+        values: Vec<String>,
+    },
+
+    AsInOfSqlValue {
+        name: &'static str,
+        values: Vec<&'s (dyn tokio_postgres::types::ToSql + Sync)>,
+    },
 }
--- src/select_builders/where_builder.rs
+++ src/select_builders/where_builder.rs
@@ -14,6 +14,7 @@ pub fn build_where<'s, TWhereModel: SqlWhereData<'s>>(
             InputDataValue::AsString { name, op, value } => {
                 sql.push_str(name);
                 sql.push_str(op);
+                sql.push_str("'");
                 sql.push_str(value.as_str());
                 sql.push_str("'");
             }
@@ -28,6 +29,82 @@ pub fn build_where<'s, TWhereModel: SqlWhereData<'s>>(
                 sql.push_str(op);
                 sql.push_str("$");
                 sql.push_str(params.len().to_string().as_str());
+            }
+            InputDataValue::AsInOfString { name, values } => {
+                if values.len() == 0 {
+                    continue;
+                }
+                if values.len() == 1 {
+                    sql.push_str(name);
+                    sql.push_str("=");
+                    sql.push_str("'");
+                    sql.push_str(values.get(0).unwrap());
+                    sql.push_str("'");
+                }
+                sql.push_str(name);
+                sql.push_str(" IN (");
+                let mut no = 0;
+                for value in &values {
+                    if no > 0 {
+                        sql.push_str(",");
+                    }
+
+                    no += 1;
+                    sql.push_str("'");
+                    sql.push_str(value);
+                    sql.push_str("'");
+                }
+                sql.push_str(")");
+            }
+            InputDataValue::AsInOfNonString { name, values } => {
+                if values.len() == 0 {
+                    continue;
+                }
+                if values.len() == 1 {
+                    sql.push_str(name);
+                    sql.push_str("=");
+                    sql.push_str(values.get(0).unwrap());
+                }
+
+                sql.push_str(name);
+                sql.push_str(" IN (");
+                let mut no = 0;
+                for value in &values {
+                    if no > 0 {
+                        sql.push_str(",");
+                    }
+
+                    no += 1;
+                    sql.push_str(value);
+                }
+                sql.push_str(")");
+            }
+            InputDataValue::AsInOfSqlValue { name, values } => {
+                if values.len() == 0 {
+                    continue;
+                }
+                if values.len() == 1 {
+                    sql.push_str(name);
+                    sql.push_str("=");
+                    sql.push('$');
+                    params.push(*values.get(0).unwrap());
+                    sql.push_str(params.len().to_string().as_str());
+                }
+
+                sql.push_str(name);
+                sql.push_str(" IN (");
+                let mut no = 0;
+                for value in values {
+                    if no > 0 {
+                        sql.push_str(",");
+                    }
+
+                    no += 1;
+                    sql.push('$');
+                    params.push(value);
+                    sql.push_str(params.len().to_string().as_str());
+                }
+                sql.push_str(")");
             }
         }
     }
