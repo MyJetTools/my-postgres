d37252ccee39c190c116953863016251202191f1
amigin <amigin@gmail.com>
2022-11-26 22:21:09 +0200

    Small Fixes

--- src/sql_select/select_part_value.rs
+++ src/sql_select/select_part_value.rs
@@ -1,95 +1,95 @@
 use rust_extensions::date_time::DateTimeAsMicroseconds;
 
 pub trait SelectPartValue {
-    fn fill(sql: &mut String, field_name: &str, sql_type: Option<&str>);
+    fn fill_select_part(sql: &mut String, field_name: &str, sql_type: Option<&str>);
 }
 
 impl SelectPartValue for String {
-    fn fill(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
+    fn fill_select_part(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
         sql.push_str(field_name);
     }
 }
 
 impl<'s> SelectPartValue for &'s str {
-    fn fill(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
+    fn fill_select_part(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
         sql.push_str(field_name);
     }
 }
 
 impl SelectPartValue for usize {
-    fn fill(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
+    fn fill_select_part(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
         sql.push_str(field_name);
     }
 }
 
 impl SelectPartValue for i64 {
-    fn fill(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
+    fn fill_select_part(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
         sql.push_str(field_name);
     }
 }
 
 impl SelectPartValue for u64 {
-    fn fill(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
+    fn fill_select_part(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
         sql.push_str(field_name);
     }
 }
 
 impl SelectPartValue for i32 {
-    fn fill(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
+    fn fill_select_part(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
         sql.push_str(field_name);
     }
 }
 
 impl SelectPartValue for u32 {
-    fn fill(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
+    fn fill_select_part(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
         sql.push_str(field_name);
     }
 }
 
 impl SelectPartValue for i16 {
-    fn fill(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
+    fn fill_select_part(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
         sql.push_str(field_name);
     }
 }
 
 impl SelectPartValue for u16 {
-    fn fill(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
+    fn fill_select_part(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
         sql.push_str(field_name);
     }
 }
 
 impl SelectPartValue for i8 {
-    fn fill(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
+    fn fill_select_part(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
         sql.push_str(field_name);
     }
 }
 
 impl SelectPartValue for u8 {
-    fn fill(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
+    fn fill_select_part(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
         sql.push_str(field_name);
     }
 }
 
 impl SelectPartValue for f64 {
-    fn fill(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
+    fn fill_select_part(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
         sql.push_str(field_name);
     }
 }
 
 impl SelectPartValue for f32 {
-    fn fill(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
+    fn fill_select_part(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
         sql.push_str(field_name);
     }
 }
 
 impl SelectPartValue for bool {
-    fn fill(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
+    fn fill_select_part(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
         sql.push_str(field_name);
     }
 }
 
 impl SelectPartValue for DateTimeAsMicroseconds {
-    fn fill(sql: &mut String, field_name: &str, sql_type: Option<&str>) {
+    fn fill_select_part(sql: &mut String, field_name: &str, sql_type: Option<&str>) {
         if let Some(sql_type) = sql_type {
             if sql_type == "bigint" {
                 sql.push_str("(extract(EPOCH FROM ");
