3057326bdbe8ec20909365f1979fc00504e84ebf
amigin <amigin@gmail.com>
2022-11-27 11:43:33 +0200

    Added Where Renderer

--- src/sql_where/mod.rs
+++ src/sql_where/mod.rs
@@ -1,8 +1,10 @@
 mod model;
 mod null_or_not_null;
+mod renderer;
 //mod sql_where_builder;
 //mod sql_where_value;
 pub use model::*;
 pub use null_or_not_null::*;
+pub use renderer::*;
 //pub use sql_where_builder::*;
 //pub use sql_where_value::*;
---
+++ src/sql_where/renderer.rs
@@ -0,0 +1,97 @@
+use crate::SqlValueWriter;
+
+pub struct WhereRenderer {
+    no: usize,
+}
+
+impl WhereRenderer {
+    pub fn new() -> Self {
+        Self { no: 0 }
+    }
+
+    fn add_delimiter(&mut self, sql: &mut String) {
+        if self.no > 0 {
+            sql.push_str(" AND ");
+        } else {
+            sql.push(' ');
+            self.no += 1;
+        }
+    }
+
+    pub fn add_value<'s, TSqlValueWriter: SqlValueWriter<'s>>(
+        &'s mut self,
+        sql: &mut String,
+        name: &'s str,
+        op: &'s str,
+        value: &'s TSqlValueWriter,
+        params: &mut Vec<&'s (dyn tokio_postgres::types::ToSql + Sync)>,
+        sql_type: Option<&'static str>,
+    ) {
+        self.add_delimiter(sql);
+        sql.push_str(name);
+        sql.push_str(op);
+        value.write(sql, params, sql_type);
+    }
+
+    pub fn add_optional_value<'s, TSqlValueWriter: SqlValueWriter<'s>>(
+        &'s mut self,
+        sql: &mut String,
+        name: &'s str,
+        op: &'s str,
+        value: &'s Option<TSqlValueWriter>,
+        params: &mut Vec<&'s (dyn tokio_postgres::types::ToSql + Sync)>,
+        sql_type: Option<&'static str>,
+    ) {
+        if let Some(value) = value {
+            self.add_value(sql, name, op, value, params, sql_type);
+        }
+    }
+
+    pub fn add_vec<'s, TSqlValueWriter: SqlValueWriter<'s>>(
+        &'s mut self,
+        sql: &mut String,
+        name: &'static str,
+        values: &Vec<&'s TSqlValueWriter>,
+        params: &mut Vec<&'s (dyn tokio_postgres::types::ToSql + Sync)>,
+        sql_type: Option<&'static str>,
+    ) {
+        if values.len() == 0 {
+            return;
+        }
+
+        if values.len() == 1 {
+            self.add_delimiter(sql);
+            sql.push_str(name);
+            sql.push('=');
+            values.get(0).unwrap().write(sql, params, sql_type);
+            return;
+        }
+
+        self.add_delimiter(sql);
+        sql.push_str(name);
+        sql.push_str(" IN (");
+        let mut no = 0;
+        for value in values {
+            if no > 0 {
+                sql.push(',');
+            }
+            no += 1;
+            value.write(sql, params, sql_type);
+        }
+
+        sql.push(')');
+    }
+
+    pub fn add_opt_of_vec<'s, TSqlValueWriter: SqlValueWriter<'s>>(
+        &'s mut self,
+        sql: &mut String,
+        name: &'static str,
+        values: &Option<Vec<&'s TSqlValueWriter>>,
+        params: &mut Vec<&'s (dyn tokio_postgres::types::ToSql + Sync)>,
+        sql_type: Option<&'static str>,
+    ) {
+        if let Some(values) = values {
+            self.add_vec(sql, name, values, params, sql_type);
+        }
+    }
+}
