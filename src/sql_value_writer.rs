05575e607acc079e8dde866038ce51a0c9775594
amigin <amigin@gmail.com>
2022-11-29 13:11:40 +0200

    Added SqlValueWriter

--- src/sql_value_writer.rs
+++ src/sql_value_writer.rs
@@ -194,3 +194,21 @@ impl<'s> SqlValueWriter<'s> for i64 {
         sql.push_str(self.to_string().as_str());
     }
 }
+
+impl<'s> SqlValueWriter<'s> for tokio_postgres::types::IsNull {
+    fn write(
+        &'s self,
+        sql: &mut String,
+        _params: &mut Vec<&'s (dyn tokio_postgres::types::ToSql + Sync)>,
+        _sql_type: Option<&'static str>,
+    ) {
+        match self {
+            tokio_postgres::types::IsNull::Yes => {
+                sql.push_str("IS NULL");
+            }
+            tokio_postgres::types::IsNull::No => {
+                sql.push_str("IS NOT NULL");
+            }
+        }
+    }
+}
