1e4b22053336b012bad1b6dc193fb108912c26bd
amigin <amigin@gmail.com>
2022-12-15 20:03:16 +0200

    Fixing BulkDelete

--- src/sql_delete/build_bulk_delete.rs
+++ src/sql_delete/build_bulk_delete.rs
@@ -10,12 +10,22 @@ pub fn build_bulk_delete<'s, TSqlWhereModel: crate::sql_where::SqlWhereModel<'s>
 
     let mut params = Vec::new();
 
-    let no = 0;
-    for where_model in where_models {
-        if no > 0 {
-            sql.push_str(" OR ");
+    if where_models.len() == 1 {
+        where_models
+            .get(0)
+            .unwrap()
+            .fill_where(&mut sql, &mut params);
+    } else {
+        let mut no = 0;
+        for where_model in where_models {
+            if no > 0 {
+                sql.push_str(" OR ");
+            }
+            sql.push('(');
+            where_model.fill_where(&mut sql, &mut params);
+            sql.push(')');
+            no += 1;
         }
-        where_model.fill_where(&mut sql, &mut params);
     }
 
     (sql, params)
