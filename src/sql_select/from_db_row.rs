f0ba8fcb93b5bbc2c2c2f40596939d0130fa10a4
amigin <amigin@gmail.com>
2022-12-29 13:36:03 +0200

    Fixes

--- src/sql_select/from_db_row.rs
+++ src/sql_select/from_db_row.rs
@@ -1,11 +1,21 @@
 use rust_extensions::date_time::DateTimeAsMicroseconds;
 
+use crate::SqlValueMetadata;
+
 pub trait FromDbRow<TResult> {
-    fn from_db_row(row: &tokio_postgres::Row, name: &str, sql_type: Option<&str>) -> TResult;
+    fn from_db_row(
+        row: &tokio_postgres::Row,
+        name: &str,
+        metadata: &Option<SqlValueMetadata>,
+    ) -> TResult;
 }
 
 impl FromDbRow<String> for String {
-    fn from_db_row(row: &tokio_postgres::Row, name: &str, _sql_type: Option<&str>) -> String {
+    fn from_db_row(
+        row: &tokio_postgres::Row,
+        name: &str,
+        _metadata: &Option<SqlValueMetadata>,
+    ) -> String {
         row.get(name)
     }
 }
@@ -14,32 +24,48 @@ impl FromDbRow<Option<String>> for Option<String> {
     fn from_db_row(
         row: &tokio_postgres::Row,
         name: &str,
-        _sql_type: Option<&str>,
+        _metadata: &Option<SqlValueMetadata>,
     ) -> Option<String> {
         row.get(name)
     }
 }
 
 impl FromDbRow<i64> for i64 {
-    fn from_db_row(row: &tokio_postgres::Row, name: &str, _sql_type: Option<&str>) -> i64 {
+    fn from_db_row(
+        row: &tokio_postgres::Row,
+        name: &str,
+        _metadata: &Option<SqlValueMetadata>,
+    ) -> i64 {
         row.get(name)
     }
 }
 impl FromDbRow<Option<i64>> for Option<i64> {
-    fn from_db_row(row: &tokio_postgres::Row, name: &str, _sql_type: Option<&str>) -> Option<i64> {
+    fn from_db_row(
+        row: &tokio_postgres::Row,
+        name: &str,
+        _metadata: &Option<SqlValueMetadata>,
+    ) -> Option<i64> {
         row.get(name)
     }
 }
 
 impl FromDbRow<u64> for u64 {
-    fn from_db_row(row: &tokio_postgres::Row, name: &str, _sql_type: Option<&str>) -> u64 {
+    fn from_db_row(
+        row: &tokio_postgres::Row,
+        name: &str,
+        _metadata: &Option<SqlValueMetadata>,
+    ) -> u64 {
         let result: i64 = row.get(name);
         result as u64
     }
 }
 
 impl FromDbRow<Option<u64>> for Option<u64> {
-    fn from_db_row(row: &tokio_postgres::Row, name: &str, _sql_type: Option<&str>) -> Option<u64> {
+    fn from_db_row(
+        row: &tokio_postgres::Row,
+        name: &str,
+        _metadata: &Option<SqlValueMetadata>,
+    ) -> Option<u64> {
         let result: Option<i64> = row.get(name);
         let result = result?;
         Some(result as u64)
@@ -47,26 +73,42 @@ impl FromDbRow<Option<u64>> for Option<u64> {
 }
 
 impl FromDbRow<i32> for i32 {
-    fn from_db_row(row: &tokio_postgres::Row, name: &str, _sql_type: Option<&str>) -> i32 {
+    fn from_db_row(
+        row: &tokio_postgres::Row,
+        name: &str,
+        _metadata: &Option<SqlValueMetadata>,
+    ) -> i32 {
         row.get(name)
     }
 }
 
 impl FromDbRow<Option<i32>> for Option<i32> {
-    fn from_db_row(row: &tokio_postgres::Row, name: &str, _sql_type: Option<&str>) -> Option<i32> {
+    fn from_db_row(
+        row: &tokio_postgres::Row,
+        name: &str,
+        _metadata: &Option<SqlValueMetadata>,
+    ) -> Option<i32> {
         row.get(name)
     }
 }
 
 impl FromDbRow<u32> for u32 {
-    fn from_db_row(row: &tokio_postgres::Row, name: &str, _sql_type: Option<&str>) -> u32 {
+    fn from_db_row(
+        row: &tokio_postgres::Row,
+        name: &str,
+        _metadata: &Option<SqlValueMetadata>,
+    ) -> u32 {
         let result: i64 = row.get(name);
         result as u32
     }
 }
 
 impl FromDbRow<Option<u32>> for Option<u32> {
-    fn from_db_row(row: &tokio_postgres::Row, name: &str, _sql_type: Option<&str>) -> Option<u32> {
+    fn from_db_row(
+        row: &tokio_postgres::Row,
+        name: &str,
+        _metadata: &Option<SqlValueMetadata>,
+    ) -> Option<u32> {
         let result: Option<i64> = row.get(name);
         let result = result?;
         Some(result as u32)
@@ -74,37 +116,61 @@ impl FromDbRow<Option<u32>> for Option<u32> {
 }
 
 impl FromDbRow<bool> for bool {
-    fn from_db_row(row: &tokio_postgres::Row, name: &str, _sql_type: Option<&str>) -> bool {
+    fn from_db_row(
+        row: &tokio_postgres::Row,
+        name: &str,
+        _metadata: &Option<SqlValueMetadata>,
+    ) -> bool {
         row.get(name)
     }
 }
 
 impl FromDbRow<Option<bool>> for Option<bool> {
-    fn from_db_row(row: &tokio_postgres::Row, name: &str, _sql_type: Option<&str>) -> Option<bool> {
+    fn from_db_row(
+        row: &tokio_postgres::Row,
+        name: &str,
+        _metadata: &Option<SqlValueMetadata>,
+    ) -> Option<bool> {
         row.get(name)
     }
 }
 
 impl FromDbRow<f64> for f64 {
-    fn from_db_row(row: &tokio_postgres::Row, name: &str, _sql_type: Option<&str>) -> f64 {
+    fn from_db_row(
+        row: &tokio_postgres::Row,
+        name: &str,
+        _metadata: &Option<SqlValueMetadata>,
+    ) -> f64 {
         row.get(name)
     }
 }
 
 impl FromDbRow<Option<f64>> for Option<f64> {
-    fn from_db_row(row: &tokio_postgres::Row, name: &str, _sql_type: Option<&str>) -> Option<f64> {
+    fn from_db_row(
+        row: &tokio_postgres::Row,
+        name: &str,
+        _metadata: &Option<SqlValueMetadata>,
+    ) -> Option<f64> {
         row.get(name)
     }
 }
 
 impl FromDbRow<f32> for f32 {
-    fn from_db_row(row: &tokio_postgres::Row, name: &str, _sql_type: Option<&str>) -> f32 {
+    fn from_db_row(
+        row: &tokio_postgres::Row,
+        name: &str,
+        _metadata: &Option<SqlValueMetadata>,
+    ) -> f32 {
         row.get(name)
     }
 }
 
 impl FromDbRow<Option<f32>> for Option<f32> {
-    fn from_db_row(row: &tokio_postgres::Row, name: &str, _sql_type: Option<&str>) -> Option<f32> {
+    fn from_db_row(
+        row: &tokio_postgres::Row,
+        name: &str,
+        _metadata: &Option<SqlValueMetadata>,
+    ) -> Option<f32> {
         row.get(name)
     }
 }
@@ -113,7 +179,7 @@ impl FromDbRow<DateTimeAsMicroseconds> for DateTimeAsMicroseconds {
     fn from_db_row(
         row: &tokio_postgres::Row,
         name: &str,
-        _sql_type: Option<&str>,
+        _metadata: &Option<SqlValueMetadata>,
     ) -> DateTimeAsMicroseconds {
         let unix_microseconds: i64 = row.get(name);
         DateTimeAsMicroseconds::new(unix_microseconds)
@@ -124,7 +190,7 @@ impl FromDbRow<Option<DateTimeAsMicroseconds>> for Option<DateTimeAsMicroseconds
     fn from_db_row(
         row: &tokio_postgres::Row,
         name: &str,
-        _sql_type: Option<&str>,
+        _metadata: &Option<SqlValueMetadata>,
     ) -> Option<DateTimeAsMicroseconds> {
         let unix_microseconds: Option<i64> = row.get(name);
         let unix_microseconds = unix_microseconds?;
