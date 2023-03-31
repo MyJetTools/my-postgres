d5226816e4a38346758f7e34433a4b4a4a68e6fd
amigin <amigin@gmail.com>
2023-03-31 18:31:25 +0300

    Fixes

--- src/table_schema/sql_type_provider.rs
+++ src/table_schema/sql_type_provider.rs
@@ -5,89 +5,89 @@ use crate::SqlValueMetadata;
 use super::TableColumnType;
 
 pub trait SqlTypeProvider {
-    fn get_sql_type(&self, metadata: Option<SqlValueMetadata>) -> TableColumnType;
+    fn get_sql_type(metadata: Option<SqlValueMetadata>) -> TableColumnType;
 }
 
 impl SqlTypeProvider for u8 {
-    fn get_sql_type(&self, _metadata: Option<SqlValueMetadata>) -> TableColumnType {
+    fn get_sql_type(_metadata: Option<SqlValueMetadata>) -> TableColumnType {
         TableColumnType::SmallInt
     }
 }
 
 impl SqlTypeProvider for i8 {
-    fn get_sql_type(&self, _metadata: Option<SqlValueMetadata>) -> TableColumnType {
+    fn get_sql_type(_metadata: Option<SqlValueMetadata>) -> TableColumnType {
         TableColumnType::SmallInt
     }
 }
 
 impl SqlTypeProvider for u16 {
-    fn get_sql_type(&self, _metadata: Option<SqlValueMetadata>) -> TableColumnType {
+    fn get_sql_type(_metadata: Option<SqlValueMetadata>) -> TableColumnType {
         TableColumnType::Integer
     }
 }
 
 impl SqlTypeProvider for i16 {
-    fn get_sql_type(&self, _metadata: Option<SqlValueMetadata>) -> TableColumnType {
+    fn get_sql_type(_metadata: Option<SqlValueMetadata>) -> TableColumnType {
         TableColumnType::SmallInt
     }
 }
 
 impl SqlTypeProvider for u32 {
-    fn get_sql_type(&self, _metadata: Option<SqlValueMetadata>) -> TableColumnType {
+    fn get_sql_type(_metadata: Option<SqlValueMetadata>) -> TableColumnType {
         TableColumnType::Integer
     }
 }
 
 impl SqlTypeProvider for i32 {
-    fn get_sql_type(&self, _metadata: Option<SqlValueMetadata>) -> TableColumnType {
+    fn get_sql_type(_metadata: Option<SqlValueMetadata>) -> TableColumnType {
         TableColumnType::Integer
     }
 }
 
 impl SqlTypeProvider for u64 {
-    fn get_sql_type(&self, _metadata: Option<SqlValueMetadata>) -> TableColumnType {
+    fn get_sql_type(_metadata: Option<SqlValueMetadata>) -> TableColumnType {
         TableColumnType::BigInt
     }
 }
 
 impl SqlTypeProvider for i64 {
-    fn get_sql_type(&self, _metadata: Option<SqlValueMetadata>) -> TableColumnType {
+    fn get_sql_type(_metadata: Option<SqlValueMetadata>) -> TableColumnType {
         TableColumnType::BigInt
     }
 }
 
 impl SqlTypeProvider for usize {
-    fn get_sql_type(&self, _metadata: Option<SqlValueMetadata>) -> TableColumnType {
+    fn get_sql_type(_metadata: Option<SqlValueMetadata>) -> TableColumnType {
         TableColumnType::BigInt
     }
 }
 
 impl SqlTypeProvider for isize {
-    fn get_sql_type(&self, _metadata: Option<SqlValueMetadata>) -> TableColumnType {
+    fn get_sql_type(_metadata: Option<SqlValueMetadata>) -> TableColumnType {
         TableColumnType::BigInt
     }
 }
 
 impl SqlTypeProvider for String {
-    fn get_sql_type(&self, _metadata: Option<SqlValueMetadata>) -> TableColumnType {
+    fn get_sql_type(_metadata: Option<SqlValueMetadata>) -> TableColumnType {
         TableColumnType::Text
     }
 }
 
 impl<T> SqlTypeProvider for Vec<T> {
-    fn get_sql_type(&self, _metadata: Option<SqlValueMetadata>) -> TableColumnType {
+    fn get_sql_type(_metadata: Option<SqlValueMetadata>) -> TableColumnType {
         TableColumnType::Json
     }
 }
 
 impl SqlTypeProvider for bool {
-    fn get_sql_type(&self, _metadata: Option<SqlValueMetadata>) -> TableColumnType {
+    fn get_sql_type(_metadata: Option<SqlValueMetadata>) -> TableColumnType {
         TableColumnType::Boolean
     }
 }
 
 impl SqlTypeProvider for DateTimeAsMicroseconds {
-    fn get_sql_type(&self, metadata: Option<SqlValueMetadata>) -> TableColumnType {
+    fn get_sql_type(metadata: Option<SqlValueMetadata>) -> TableColumnType {
         if let Some(metadata) = metadata {
             if let Some(sql_type) = metadata.sql_type {
                 if sql_type == "timestamp" {
