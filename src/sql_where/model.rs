2874137c4778ac6e7ceb1e169995a065a13ee77c
amigin <amigin@gmail.com>
2023-06-20 11:13:28 +0300

    Debugging

--- src/sql_where/model.rs
+++ src/sql_where/model.rs
@@ -1,18 +1,14 @@
 use crate::{SqlValue, SqlValueMetadata, SqlWhereValueWriter};
 
-pub enum WhereFieldDataWrapper<'s> {
-    Ignore,
-    Value(WhereFieldData<'s>),
-}
-
 pub struct WhereFieldData<'s> {
     pub field_name: &'s str,
     pub op: Option<&'static str>,
-    pub value: &'s dyn SqlWhereValueWriter<'s>,
+    pub ignore_if_none: bool,
+    pub value: Option<&'s dyn SqlWhereValueWriter<'s>>,
     pub meta_data: Option<SqlValueMetadata>,
 }
 pub trait SqlWhereModel<'s> {
-    fn get_where_field_name_data(&'s self, no: usize) -> Option<WhereFieldDataWrapper<'s>>;
+    fn get_where_field_name_data(&'s self, no: usize) -> Option<WhereFieldData<'s>>;
 
     fn get_limit(&self) -> Option<usize>;
     fn get_offset(&self) -> Option<usize>;
@@ -23,28 +19,38 @@ pub trait SqlWhereModel<'s> {
         let mut rendered_no = 0;
 
         while let Some(field_data) = self.get_where_field_name_data(no) {
-            match field_data {
-                WhereFieldDataWrapper::Ignore => {
-                    no += 1;
-                }
-                WhereFieldDataWrapper::Value(field_data) => {
+            if field_data.value.is_none() && field_data.ignore_if_none {
+                if !field_data.ignore_if_none {
                     if rendered_no > 0 {
                         sql.push_str(" AND ");
                     } else {
                         sql.push_str(" WHERE ");
                     }
 
-                    no += 1;
-                    rendered_no += 1;
                     sql.push_str(field_data.field_name);
-                    if let Some(op) = field_data.op {
-                        sql.push_str(op);
-                    } else {
-                        sql.push_str(field_data.value.get_default_operator());
-                    }
-                    field_data.value.write(sql, params, &field_data.meta_data);
+                    sql.push_str(" IS NULL")
                 }
+                no += 1;
+                continue;
             }
+
+            let value = field_data.value.unwrap();
+
+            if rendered_no > 0 {
+                sql.push_str(" AND ");
+            } else {
+                sql.push_str(" WHERE ");
+            }
+
+            no += 1;
+            rendered_no += 1;
+            sql.push_str(field_data.field_name);
+            if let Some(op) = field_data.op {
+                sql.push_str(op);
+            } else {
+                sql.push_str(value.get_default_operator());
+            }
+            value.write(sql, params, &field_data.meta_data);
         }
     }
 
