b223ea9e414b1217163e3b4b82219194dfc46080
amigin <amigin@gmail.com>
2023-06-21 12:46:04 +0300

    Added RelatedFieldName

--- src/sql_value/sql_value_metadata.rs
+++ src/sql_value/sql_value_metadata.rs
@@ -1,11 +1,4 @@
 pub struct SqlValueMetadata {
     pub sql_type: Option<&'static str>,
-}
-
-impl SqlValueMetadata {
-    pub fn with_sql_type(sql_type: &'static str) -> Self {
-        Self {
-            sql_type: Some(sql_type),
-        }
-    }
+    pub related_field_name: Option<&'static str>,
 }
