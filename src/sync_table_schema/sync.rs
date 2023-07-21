bf60ca692af57dcffbc3f4327bd0eace47e72d01
amigin <amigin@gmail.com>
2023-07-21 20:21:22 +0300

    Debugging

--- src/sync_table_schema/sync.rs
+++ src/sync_table_schema/sync.rs
@@ -9,37 +9,37 @@ pub async fn sync_schema(
         dyn rust_extensions::Logger + Sync + Send + 'static,
     >,
 ) -> Result<(), MyPostgresError> {
+    if let Err(error) = super::check_if_db_exists(
+        conn_string,
+        #[cfg(feature = "with-logs-and-telemetry")]
+        logger,
+    )
+    .await
+    {
+        println!(
+            "Can not execute script which checks DataBase existence. Error: {:?}",
+            error
+        );
+
+        #[cfg(feature = "with-logs-and-telemetry")]
+        {
+            let mut ctx = std::collections::HashMap::new();
+
+            ctx.insert("Err".to_string(), format!("{:?}", error));
+
+            logger.write_info(
+                "Table Existence verification".into(),
+                format!("Can not execute script which checks DataBase existence",),
+                Some(ctx),
+            );
+        }
+    }
+
     loop {
         println!("--------------------------------------------------");
         println!("Syncing table schema: {}", table_schema.table_name);
         if std::env::var("DEBUG").is_ok() {
             tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-        }
-
-        if let Err(error) = super::check_if_db_exists(
-            conn_string,
-            #[cfg(feature = "with-logs-and-telemetry")]
-            logger,
-        )
-        .await
-        {
-            println!(
-                "Can not execute script which checks DataBase existence. Error: {:?}",
-                error
-            );
-
-            #[cfg(feature = "with-logs-and-telemetry")]
-            {
-                let mut ctx = std::collections::HashMap::new();
-
-                ctx.insert("Err".to_string(), format!("{:?}", error));
-
-                logger.write_info(
-                    "Table Existence verification".into(),
-                    format!("Can not execute script which checks DataBase existence",),
-                    Some(ctx),
-                );
-            }
         }
 
         if super::sync_table_fields(
