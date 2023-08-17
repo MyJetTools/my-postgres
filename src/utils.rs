3e5dbb27dd183848ec31d26b0f5cefa4bea9dbb3
amigin <amigin@gmail.com>
2023-08-17 20:36:12 +0300

    Added parsing case_and_model

--- src/utils.rs
+++ src/utils.rs
@@ -6,3 +6,28 @@ pub fn compile_enum_with_model(case: &str, volume: &str) -> String {
 
     String::from_utf8(json_writer.build()).unwrap()
 }
+
+pub fn get_case_and_model<'s>(value: &'s str) -> (&'s str, &'s str) {
+    let mut case = None;
+    let mut model = None;
+
+    for itm in my_json::json_reader::JsonFirstLineReader::new(value.as_bytes()) {
+        let itm = itm.unwrap();
+
+        match itm.get_name().unwrap() {
+            "case" => case = Some(itm.get_value().unwrap().as_str().unwrap()),
+            "model" => model = Some(itm.get_value().unwrap().as_str().unwrap()),
+            _ => {}
+        }
+    }
+
+    if case.is_none() {
+        panic!("Can't find case in {}", value);
+    }
+
+    if model.is_none() {
+        panic!("Can't find model in {}", value);
+    }
+
+    (case.unwrap(), model.unwrap())
+}
